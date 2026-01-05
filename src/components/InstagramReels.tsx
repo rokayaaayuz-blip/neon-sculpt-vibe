020db5f2d5a19724f8e54da263263ecfaa24ebf0
rokayaaayuz-blip <rokayaaayuz@gmail.com>
2026-01-05 17:09:47 +0530

    Update InstagramReels.tsx

--- src/components/InstagramReels.tsx
+++ src/components/InstagramReels.tsx
@@ -3,8 +3,9 @@ import ElfsightInstagramFeed from "./ElfsightInstagramFeed";
 
 const InstagramReels = () => {
   return (
-    <section className="py-24 bg-card/30">
-      <div className="container mx-auto px-4">
+    <section className="py-24 bg-card/30 w-full overflow-hidden">
+      {/* We removed the 'container' class and used max-w-5xl (1024px) to ensure 4xl width */}
+      <div className="max-w-[1024px] mx-auto px-4">
         <div className="text-center mb-12">
           <div className="flex items-center justify-center gap-3 mb-4">
             <Instagram className="w-8 h-8 text-primary" />
@@ -17,8 +18,10 @@ const InstagramReels = () => {
           </p>
         </div>
         
-        {/* Elfsight Instagram Feed Widget */}
-        <ElfsightInstagramFeed />
+        {/* This will now be able to fill the 1024px space */}
+        <div className="w-full">
+          <ElfsightInstagramFeed />
+        </div>
         
         <div className="text-center mt-8">
           <a
